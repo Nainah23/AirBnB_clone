b658ea9e7d9e99e81e058fd840c1358f0a561e8a
Nainah23 <kamauwainaina29@gmail.com>
2024-02-10 02:45:17 -0800

    Update console

--- console.py
+++ console.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
+
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """My class"""
+    """My custom console class"""
     intro = "Welcome to our AirBnB Console. Type 'help' for commands\n"
     prompt = "(hbnb) "
 
 
-    def do_EOF(self, args):
-        """Signifies end-of-file"""
-        return True
     def do_quit(self, args):
         """Quits the program"""
         return True
+    def do_EOF(self, args):
+        """Signifies end-of-file"""
+        return True
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
