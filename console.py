e40f8b193d7b10c7ae4957a33ae9e7f17431db07
jaymfune <jacobmfune2@gmail.com>
2024-02-10 15:25:16 +0200

    console.py fix

--- console.py
+++ console.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
-"""Module for HBNB command interpreter"""
+
+"""
+HBNB Console Module
+
+This module defines a custom console class, HBNBCommand, for the AirBnB application.
+
+Attributes:
+    prompt (str): Custom prompt for the console.
+
+Methods:
+    do_quit(self, args): Quits the program.
+    do_EOF(self, args): Handles end-of-file event.
+
+Usage:
+    Execute this script to start the AirBnB console. Type 'help' for available commands.
+"""
 
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter class"""
-
+    """Our custom console class"""
     prompt = "(hbnb) "
 
-    def do_quit(self, arg):
-        """Quit command to exit the program"""
+    def do_quit(self, args):
+        """Quits the program"""
         return True
 
-    def help_quit(self):
-        """Prints help for quit command"""
-        print("Quit command to exit the program")
-
-    def do_EOF(self, arg):
-        """EOF command to exit the program"""
-        print("")
+    def do_EOF(self, args):
+        """Signifies end-of-file"""
+        print()
         return True
-
-    def help_EOF(self):
-        """Prints help for EOF command"""
-        print("EOF command to exit the program")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
