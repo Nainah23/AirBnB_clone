9034e12afb087698c4c80f36a9da15a59143c66a
Nainah23 <kamauwainaina29@gmail.com>
2024-02-10 04:51:18 -0800

    Update console

--- console.py
+++ console.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 
+"""
+HBNB Console Module
+
+This module defines a custom console class, HBNBCommand, for the AirBnB application.
+
+Attributes:
+    prompt (str): Custom prompt for the console.
+
+Methods:
+    do_quit(self, args): Quits the program.
+    do_EOF(self, args): Handles end-of-file event.
+
+Usage:
+    Execute this script to start the AirBnB console. Type 'help' for available commands.
+"""
+
 import cmd
 
 class HBNBCommand(cmd.Cmd):
